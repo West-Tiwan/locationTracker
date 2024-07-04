6d828d24ab64d27ec87075711c36882cdc934271
Mohit Singh <xyzmohit.singh@gmail.com>
2024-07-05 00:58:58 +0530

    socket comms wokring but only 1 user visible

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const path = require("path");
+
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 
-app.get("/",(req,res)=>{
-    res.send("hi");
-})
+app.set("view engine", "ejs");
+app.use('/scripts', express.static(path.join(__dirname, 'public/scripts')));
+app.use('/styles', express.static(path.join(__dirname, 'public/styles')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(3000,()=>{
-    console.log("listening on port 3000");
-})
+io.on("connection", (socket) => {
+  console.log(`A user connected: ${socket.id}`);
+
+  socket.on("send-loc", (data) => {
+    const { latitude, longitude } = data;
+    console.log(`Location from ${socket.id}: ${latitude}, ${longitude}`);
+
+    io.emit("receive-loc", { id: socket.id, latitude, longitude });
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`User disconnected: ${socket.id}`);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+server.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
---
+++ public/scripts/script.js
@@ -0,0 +1,49 @@
+const socket = io();
+
+if (navigator.geolocation) {
+  navigator.geolocation.watchPosition(
+    (position) => {
+      const { latitude, longitude } = position.coords;
+      socket.emit("send-loc", { latitude, longitude });
+    },
+    (error) => {
+      console.error("Geolocation error:", error);
+    },
+    {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    }
+  );
+} else {
+  console.error("Geolocation is not supported by this browser.");
+}
+
+const map = L.map("map").setView([0, 0], 16);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "OpenStreetMap",
+}).addTo(map);
+
+const markers = {};
+
+socket.on("receive-loc", (data) => {
+  const { id, latitude, longitude } = data;
+  console.log(`Received location update for ${id}: ${latitude}, ${longitude}`);
+
+  if (markers[id]) {
+    markers[id].setLatLng([latitude, longitude]);
+  } else {
+    markers[id] = L.marker([latitude, longitude]).addTo(map);
+  }
+
+  map.setView([latitude, longitude], 10);
+});
+
+
+socket.on("disconnect",(id)=>{
+  if(markers[id]){
+    map.removeLayer(markers[id]);
+    delete markers[id];
+  }
+})
